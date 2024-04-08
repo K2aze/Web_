4b02a580410a684124264de4156e927c9dcbc717
k2aze <killallstart@gmail.com>
2024-04-09 08:22:08 +0900

    Prices update

--- todo_list/src/Primary.tsx
+++ todo_list/src/Primary.tsx
@@ -1,38 +1,31 @@
 function Primary() {
-    // Price
-    const Vip1 = '$15.99/month'
-    const Vip1Description = 'Experience the service with no ads and automatic storage on cloud data'
-    const Vip2 = '$13.99/month'
-    const Vip2Description = '10GB cloud storage, 3 months for only ${Vip2} per month'
-    const Vip3 = '$10/month'
-    const Vip3Description = 'Buy 10 months get 2 more months, 1-year premium offer for only ${Vip3} per month'
     
-    // Premium Prices
-    // const PremiumPrices = () => {
-    //     return (
-    //         <div>
-
-    //         </div>
-    //     )
-    // }
-
     return ( 
-        <div className="h-dvh inline-flex justify-evenly items-center">
-            <div className=" w-1/5 h-3/6 bg-gray-100 rounded-lg shadow-md p-6 flex flex-col items-center justify-between">
-                <p>Professional</p>
-                <p>{Vip1}</p>
-                <p>{Vip1Description}</p>
+        <div className="h-dvh flex flex-col items-center justify-evenly">
+        <div>
+            <div className=" text-center flex flex-col">
+                <p className=" text-blue-600">Pricing</p>
+                <h2 className=" text-3xl">Pricing plans for teams of all sizes</h2>
             </div>
-            <div className="w-1/5 h-3/6 bg-gray-100 rounded-lg shadow-md p-6 flex flex-col items-center justify-between">
-                <p>Professional</p>
-                <p>{Vip2}</p>
-                <p>{Vip2Description}</p>
+            <div>
+                <p className=" text-gray-800">Choose an affordable plan that's packed with the best features for engaging your audience, creating customer loyalty, and driving sales.</p>
             </div>
-            <div className="w-1/5 h-3/6 bg-gray-100 rounded-lg shadow-md p-6 flex flex-col items-center justify-between">
-                <p>Professional</p>
-                <p>{Vip3}</p>
-                <p>{Vip3Description}</p>
+        </div>
+        <div>
+            <span>Selection</span>
+        </div>
+        <div> 
+            <div>
+                <h3>Freelancer</h3>
             </div>
+            <div>
+                <h3>Startup</h3>
+                <p>Most popular</p>
+            </div>
+            <div>
+                <h3>Enterprise</h3>
+            </div>
+        </div>
         </div>
      );
 }
